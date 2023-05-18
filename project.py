197fcf73c0da42d7785a4eb2ea3d78006b5af8a8
DJS <demianjuarezsaldana@gmail.com>
2023-05-17 21:38:43 -0300

    limpiando y finishing

--- project.py
+++ project.py
@@ -2,40 +2,74 @@
 
 import dns.resolver
 
+def option1():
+    tiposRegistro = ['A', 'AAAA', 'NS', 'MX', 'SOA', 'TXT']
 
-option = input("Elija la configuración:\n1. 'A', 'AAAA', 'NS', 'MX', 'SOA', 'TXT'\n2. 'PTR'\n3. 'CNAME'\nElija la configuración: ")
+    try:
+        dominio = input("Ingrese dominio: ")
+    except IndexError:
+        return 'Syntax Error'
+    for registro in tiposRegistro:
+        try:
+            respuesta = dns.resolver.resolve(dominio, registro)
+            print(f'\n{registro} Registros')
+            print('-' * 30)
+            for server in respuesta:
+                print(server.to_text())
+        except dns.resolver.NoAnswer:
+            pass
+        except dns.resolver.NXDOMAIN:
+            return f'{dominio} no existe'
+        except KeyboardInterrupt:
+            return 'No apretes cualquier cosa'
+
+def option2():
+    try:
+        direccionIP = input("Ingrese direccion IP: ")
+    except IndexError:
+        return 'Syntax Error'
+    try:
+        dnsReserved = dns.reversename.from_address(direccionIP)
+        answs = dns.resolver.resolve(dnsReserved, 'PTR')
+        print(f'\nPTR Registros')
+        print('-' * 30)
+        for answ in answs:
+            print(answ.target)
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.resolver.NXDOMAIN:
+        print(f'la direccion {direccionIP} no existe')
+    except KeyboardInterrupt:
+        return "No apretes cualquier cosa"
+
+def option3():
+    try:
+        dominio = input("Ingrese dominio: ")
+    except IndexError:
+        return 'Syntax Error'
+    try:
+        answers = dns.resolver.resolve(dominio, 'CNAME')
+        for rdata in answers:
+            print(f'Registro CNAME encontrado para {dominio}: {rdata.target}')
+    except dns.resolver.NoAnswer:
+        print(f'No se encontró el registro CNAME para {dominio}')
+    except dns.resolver.NXDOMAIN:
+        print(f'El dominio {dominio} no existe')
+    except dns.exception.DNSException as e:
+        print(f'Ocurrió un error: {str(e)}')
+    except KeyboardInterrupt:
+        return "No apretes cualquier cosa"
 
 def chooser(option):
     if option == "1":
         option1()
     elif option == "2":
-        return "gay2"
+        option2()
     elif option == "3":
-        return "gay3"
+        option3()
 
+option = input("Elija la configuración:\n1. 'A', 'AAAA', 'NS', 'MX', 'SOA', 'TXT'\n2. 'PTR'\n3. 'CNAME'\nElija la configuración: ")
 resultado = chooser(option)
 print(resultado)
 
-def option1():
-    tiposRegistro = ['A','AAAA','NS','MX','SOA','TXT']
-
-    try:
-        dominio = input("Ingrese dominio: ")
-    except IndexError:
-        return'Syntax Error'
-        quit()
-    for registro in tiposRegistro:
-        try:
-            respuesta = dns.resolver.resolve(dominio, registro)
-            return f'\n{registro} Registros'
-            return '-'*30
-            for server in respuesta:
-                return server.to_text()
-        except dns.resolver.NoAnswer:
-            pass
-        except dns.resolver.NXDOMAIN:
-            print(f'{dominio} no existe')
-            quit()
-        except KeyboardInterrupt:
-            print('no apretes cualquier cosa')
-            quit()
+#No comprendo porque returnea none algunas veces en algunas option()
